62c47e58573d1d6480a5d2e04e2394466ac18b5e
Laurynasjoksas88 <144778600+Laurynasjoksas88@users.noreply.github.com>
2023-12-05 23:28:36 +0200

    update

---
+++ index.js
@@ -0,0 +1,48 @@
+"use strict";
+// pirma užduotis
+
+// let neigiamas = null;
+
+// for (let i = 0; i < 5; i++) {
+    
+// let skaicius = parseInt(prompt('Veskite skaičių: (Bus galima vesti penkis kartus)'));
+//     if (skaicius < 0) {
+//         neigiamas = skaicius;
+//         break;
+//     } else if (skaicius = 0) {
+//         console.log('0 nera nei teigiamas nei neigiamas')
+//     }
+
+// }
+
+// if (neigiamas !== null) {
+//     console.log(neigiamas);
+// } else 
+// console.log('Sistema nerado neigiamo skaičio')
+
+// antra užduotis
+
+// for (let i=1; i <= 100; i++)
+
+// {
+// niekaip neradau googlei kaip padaryti kad rodytu skaicius be liekanos, arba jei ir radau nelabai supratau kur iterpti, tikriausiai kazkur i 29 eilute
+// if (i % 3 == 0)
+
+// console.log(i);
+
+// }
+
+// trečia užduotis
+hgdfgdfgdf
+
+
+
+
+
+
+
+
+
+
+
+
